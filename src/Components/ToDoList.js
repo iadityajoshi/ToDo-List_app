a1042797b7b0ee2926a2d054b382a939c66c69af
Aditya <813aditya@gmail.com>
2023-06-04 18:18:34 +0530

    basic css changes

--- src/Components/ToDoList.js
+++ src/Components/ToDoList.js
@@ -1,51 +1,49 @@
-
-import React, { useState } from 'react'
+import React, { useState } from "react";
 // import ModalWindow from './ModalWindow';
-import Modal from './Modal';
+import Modal from "./Modal";
 
-
-const ToDoList = ( {data, clickHandler, modalSaveHandler} ) => {
-
-  const [showModal,setShowModal] = useState(false);
+const ToDoList = ({ data, clickHandler, modalSaveHandler }) => {
+  const [showModal, setShowModal] = useState(false);
   const [currEditID, setCurrEditID] = useState();
   const [content, setContent] = useState();
 
   const deleteList = (input) => {
-      clickHandler(input);
-  }
-  
+    clickHandler(input);
+  };
+
   const editHandler = (id, title) => {
-      setShowModal(true);
-      setCurrEditID(id);
-      setContent(title);    
-  }
+    setShowModal(true);
+    setCurrEditID(id);
+    setContent(title);
+  };
 
   const modalClose = () => {
-      setShowModal(false);
-  }
- 
-  // console.log(content);
+    setShowModal(false);
+  };
+  
   return (
-    <div>
-       {data.map((item) => (
-        <li key={item.id} className='list'>{item.title}
-        <div>
+    <>
+      {data.map((item) => (
+        <li key={item.id} className='list'>
+        <span className="list-title">{item.title}</span>
+        <div className="button-container">
           <button onClick={ () => {deleteList(item.id)}}  className='listButtons'>Delete</button>
           <button className='listButtons' onClick={ ()=> {editHandler(item.id, item.title)} }>Edit</button>
         </div>
         </li>
-        ) )}
-        {/* <ModalWindow id={currEditID} showStatus={showModal} /> */}
-        <Modal  id={currEditID} 
-                showStatus={showModal} 
-                modalContent={content} 
-                modalClose={modalClose} 
-                modalSaveHandler={modalSaveHandler}  
-                setModalContent = {setContent}
-        />
-    </div>
-  )
-}
+      ))}
 
-export default ToDoList
+      {/* <ModalWindow id={currEditID} showStatus={showModal} /> */}
+      <Modal
+        id={currEditID}
+        showStatus={showModal}
+        modalContent={content}
+        modalClose={modalClose}
+        modalSaveHandler={modalSaveHandler}
+        setModalContent={setContent}
+      />
+    </>
+  );
+};
 
+export default ToDoList;
